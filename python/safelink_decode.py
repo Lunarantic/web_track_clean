bdfa13674e6903a7e8a5e481abadbf87b7c8d901
Lunarantic <26676464+Lunarantic@users.noreply.github.com>
2018-12-06 16:07:39 -0500

    Added support for exception for not dropping
    Added support to not drop arguments of URLs in exception's list

--- python/safelink_decode.py
+++ python/safelink_decode.py
@@ -1,25 +1,57 @@
 #!/usr/local/bin/python3
-  
+
 from urllib.parse import urlsplit
 from urllib.parse import parse_qs
 from urllib.parse import urlunsplit
 import sys
 
-def parse(safelink):
-        print(urlunsplit(list(urlsplit(parse_qs(urlsplit(safelink).query)['url'][0]))[:3]+['','']))
-        pass
 
-def parselist(linklist):
-        for link in linklist:
-                parse(link)
-                print()
-        pass
+keep_args = ['www.youtube.com']
+debug = False
+
+
+def parse(safe_link):
+	us = urlsplit(safe_link)
+	if debug:
+		print(us)
+	usq = us.query
+	if debug:
+		print(usq)
+	pq = parse_qs(usq)
+	if debug:
+		print(pq)
+	url = pq['url']
+	if debug:
+		print(url)
+	url0 = url[0]
+	if debug:
+		print(url0)
+	us = urlsplit(url0)
+	if debug:
+		print(us)
+	if us.netloc in keep_args:
+		print(url0)
+		return
+	else:
+		usq = list(us)[:3]
+		if debug:
+			print(usq)
+		uus = urlunsplit(usq+['', ''])
+		print(uus)
+	pass
+
+
+def parse_link_list(link_list):
+	for link in link_list:
+		parse(link)
+		print()
+	pass
+
 
 if __name__=="__main__":
-        if len(sys.argv) < 2:
-                print('Error')
+	if len(sys.argv) < 2:
+		print('Error')
 
-        print()
-        parselist(sys.argv[1:])
+	print()
+	parse_link_list(sys.argv[1:])
 
-# Just looking
